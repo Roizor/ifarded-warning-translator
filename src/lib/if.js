5935c27c639ba7584e2de3e76ddf86276ca28c51
Roizor <aidenjs9@icloud.com>
2023-07-08 14:04:55 -0500

    Add translator and consturcotr

--- src/lib/if.js
+++ src/lib/if.js
@@ -6,5 +6,33 @@ const iF = {
 	 * Example: W3VIB!M+1H
 	 */
 	translate: (warnString) => {
+		const warnLevel = warnString.split('W')[1].split('')[0];
+		const isVoice = warnString.includes('V');
+		const isIdentifiable = warnString.includes('I');
+		const isBannable = warnString.includes('B');
+		const isMutable = warnString.includes('M');
+		const isInstant = warnString.includes('!');
+		const isTimed = warnString.split('+')[1] ? warnString.split('+')[1] : false;
+		
+		const obj = Object.assign({}, {
+			warnLevel,
+			isVoice,
+			isIdentifiable,
+			isBannable,
+			isMutable,
+			isInstant,
+			isTimed
+		})
+		console.log(obj)
+	},
+
+	construct: (warnLevel = "0", voice = false, identifiable = false, bannable = false, mutable = false, instant = false, time = "") => {
+		if (voice) voice = "V";
+		if (identifiable) identifiable = "I";
+		if (bannable) bannable = "B";
+		if (mutable) mutable = "M";
+		if (instant) instant = "!";
+		if (time) time = "+" + time;
+		return `W${warnLevel}${voice}${identifiable}${bannable}${mutable}${instant}${time}`;
 	}
 }
